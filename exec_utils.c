438c1a75d0d842dd00e170bd1eda24bfa4a62f80
Yop <menahem123@gmail.com>
2025-11-26 09:47:51 +0200

    remove jobs add

--- exec_utils.c
+++ exec_utils.c
@@ -1,76 +1,55 @@
 #include "exec_utils.h"
-#include <stdio.h>   // printf, fprintf
-#include <stdlib.h>  // exit
-#include <unistd.h>  // fork, execvp
-#include <sys/wait.h> // waitpid
-#include <errno.h> // errno
-#include <signal.h>  // kill (optional, for background error handling)
+#include <stdio.h>     // fprintf
+#include <stdlib.h>    // exit
+#include <unistd.h>    // fork, execvp
+#include <sys/wait.h>  // waitpid
+#include <errno.h>     // errno
 
-// Print: hwshell: <name> failed, errno is <errno>
+// Print: hw1shell: <name> failed, errno is <errno>
 void print_syscall_error(const char *name)
 {
-    fprintf(stderr, "hwshell: %s failed, errno is %d\n", name, errno);
+    fprintf(stderr, "hw1shell: %s failed, errno is %d\n", name, errno);
 }
 
-int run_foreground(char *argv[], const char *cmdline)
+/*
+ * run_external:
+ *  - argv      : command + args, NULL-terminated
+ *  - background: 0 = foreground, 1 = background
+ *
+ * Return value convention (as expected by main.c):
+ *  - if background == 0:
+ *        return 0 on success, <0 on error
+ *  - if background == 1:
+ *        return child PID (>0) on success, <0 on error
+ */
+int run_external(char *argv[], int background)
 {
-    (void)cmdline;
-
-
     pid_t pid = fork();
-    if (pid < 0) 
-    {
+    if (pid < 0) {
+        // fork failed
         print_syscall_error("fork");
         return -1;
-    } 
-    if (pid == 0)
-    {
+    }
+
+    if (pid == 0) {
         // Child process
         execvp(argv[0], argv);
-        
-         // If execvp returns, an error occurred
-        fprintf(stderr, "hwshell: invalid command\n");
+
+        // If execvp returns, an error occurred
+        fprintf(stderr, "hw1shell: invalid command\n");
         exit(1);
-        
-    } 
+    }
 
     // Parent process
-    int status;
-    if (waitpid(pid, &status, 0) < 0) 
-        {
+    if (background) {
+        // Do NOT wait; main() will store pid in its bg table
+        return pid;
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) < 0) {
             print_syscall_error("waitpid");
             return -1;
         }
-
-    return 0;
+        return 0;
+    }
 }
-
-int run_background(char *argv[], const char *cmdline)
-{
-    pid_t pid = fork();
-    if (pid <0)
-    {
-        print_syscall_error("fork");
-        return -1;
-    }
-
-    if (pid == 0)
-    {
-        // Child process
-        execvp(argv[0], argv);
-        
-        // If execvp returns, an error occurred
-        fprintf(stderr, "hwshell: invalid command\n");
-        exit(1);
-    }
-    
-    if (jobs_add(pid, cmdline) < 0)
-    {
-        fprintf(stderr, "hwshell: too many background commands running\n");
-        return -1;
-    }
-    printf("hwshell: pid %d started\n",pid);
-
-    return 0;
-
-  }
