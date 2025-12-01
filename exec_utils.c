a91dbd0a76c5e611cf8f1dc6dbcc24911ed15612
eivry-github <eivry@gmail.com>
2025-12-01 11:41:59 +0200

    changed  some comments

--- exec_utils.c
+++ exec_utils.c
@@ -1,11 +1,11 @@
 #include "exec_utils.h"
-#include <stdio.h>     // fprintf
-#include <stdlib.h>    // exit
-#include <unistd.h>    // fork, execvp
-#include <sys/wait.h>  // waitpid
-#include <errno.h>     // errno
+#include <stdio.h>     //fprintf
+#include <stdlib.h>    //exit
+#include <unistd.h>    //fork,execvp
+#include <sys/wait.h>  //waitpid
+#include <errno.h>     //errno
 
-// Print: hw1shell: <name> failed, errno is <errno>
+//print: hw1shell: <name> failed, errno is <errno>
 void print_syscall_error(const char *name)
 {
     fprintf(stderr, "hw1shell: %s failed, errno is %d\n", name, errno);
@@ -26,24 +26,24 @@ int run_external(char *argv[], int background)
 {
     pid_t pid = fork();
     if (pid < 0) {
-        // fork failed
+        //fork failed
         print_syscall_error("fork");
         return -1;
     }
 
     if (pid == 0) {
-        // Child process
+        //child process
         execvp(argv[0], argv);
 
-        // If execvp returns, an error occurred
+        //if execvp returns, an error occurred
         fprintf(stderr, "hw1shell: invalid command\n");
         print_syscall_error("execvp");
         exit(1);
     }
 
-    // Parent process
+    //parent process
     if (background) {
-        // Do NOT wait; main() will store pid in its bg table
+        //dont wait; main() will store pid in its bg table
         return pid;
     } else {
         int status;
--- exec_utils.h
+++ exec_utils.h
@@ -3,16 +3,16 @@
 
 #include <sys/types.h> 
 
-// Declared in jobs.c, We only use it here.
+//declared in jobs.c,only used here.
 int jobs_add(pid_t pid, const char *cmdline);
 
-// Run a command in the foreground (shell waits for it to finish).
+//run a command in the foreground (shell waits for it to finish).
 int run_foreground(char *argv[], const char *cmdline);
 
-// Run a command in the background (shell does NOT wait).
+//run a command in the background (shell doesnt wait).
 int run_background(char *argv[], const char *cmdline);
 
-// Print an error for a failed system call, including errno.
+//print an error for a failed system call, including errno.
 void print_syscall_error(const char *name);
 
 #endif //EXEC_UTILS_H
